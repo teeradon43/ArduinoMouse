229d79f0b93e8cb961f6fe357cb5be7ce0e248a2
Teeradon <51240166+teeradon43@users.noreply.github.com>
2022-01-06 18:09:40 +0700

    STM32F429

---
+++ STM32F4/Debug/Drivers/ILI9341/subdir.mk
@@ -0,0 +1,26 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Drivers/ILI9341/ILI9341_GFX.c \
+../Drivers/ILI9341/ILI9341_STM32_Driver.c \
+../Drivers/ILI9341/ILI9341_Touchscreen.c 
+
+OBJS += \
+./Drivers/ILI9341/ILI9341_GFX.o \
+./Drivers/ILI9341/ILI9341_STM32_Driver.o \
+./Drivers/ILI9341/ILI9341_Touchscreen.o 
+
+C_DEPS += \
+./Drivers/ILI9341/ILI9341_GFX.d \
+./Drivers/ILI9341/ILI9341_STM32_Driver.d \
+./Drivers/ILI9341/ILI9341_Touchscreen.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Drivers/ILI9341/%.o: ../Drivers/ILI9341/%.c Drivers/ILI9341/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F429xx -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -I"D:/CE/3-1/01076022 Microcontroller Application and Development/CubeIDE/429_SPI_ILLI9341/Drivers/ILI9341" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
